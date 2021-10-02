afce25e947b3959210320542ae8d4bd26bd58772
Sakthivel Balasubramanian <sakthib1@in.ibm.com>
2021-10-02 18:49:04 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -31,6 +31,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     // const location ='boston'
     // console.log('http://localhost:3000/weather?address=' + location)
+    
     // fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
